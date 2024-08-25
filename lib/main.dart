cebe98ef4b5d335e372d32830e956bcf598d2526
Sachin <s4sachinjadhav@gmail.com>
2024-08-25 23:14:28 +0530

    black screen issue resolved

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:groceries_module/ui_design/bottom_navigation/bottom_navigation_page.dart';
 
-import 'ui_design/home_pages/groceries_home_page.dart';
 import 'util/size_config.dart';
 
 void main() {
+  print("App is working");
   runApp(const MyApp());
 }
 
@@ -16,14 +16,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Grocery',
     theme: ThemeData(
       textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
         useMaterial3: true,
       ),
       home: LayoutBuilder(builder: (context, constraints) {
         return OrientationBuilder(builder: (context, orientation) {
-          SizeConfig.init(constraints, orientation);
+          if (constraints.maxWidth == 0 || constraints.maxHeight == 0) {
+            return const SizedBox();
+          } else {
+            SizeConfig.init(constraints, orientation);
+          }
           return const BottomNavigationTabPage();
         });
       }),
